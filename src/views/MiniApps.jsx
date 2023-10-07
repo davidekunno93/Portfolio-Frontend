ce731df0995d98271989324f4475c913d7efe91b
David Ekunno <matramere@gmail.com>
2023-10-06 22:54:10 -0500

    scroll reset when component change

--- src/views/MiniApps.jsx
+++ src/views/MiniApps.jsx
@@ -21,7 +21,7 @@ const MiniApps = (props) => {
                     </div>
                     <p className="m0 square-title large center-text my-1 dark-text"><strong>TimeZone App</strong></p>
                     <p className="m0 square-desc medium center-text black-text">Use this app to search the time of any city in the world!</p>
-                    <Link to="/showPage2"><button className="square-btn-big center x-large mt-3">
+                    <Link to="/showPage2"><button onClick={() => {resetScroll()}} className="square-btn-big center x-large mt-3">
                         <p className="m0 inline v-align mb-2 ml-3-4-respond">View</p>
                         <span className="material-symbols-outlined v-align ml-2 mb-2 white-text mt-h-respond">
                             arrow_forward
@@ -34,7 +34,7 @@ const MiniApps = (props) => {
                     </div>
                     <p className="m0 square-title large center-text my-1 dark-text"><strong>Pick A Pokemon</strong></p>
                     <p className="m0 square-desc medium center-text black-text">Search any pokemon and it shall appear, returning a picture and some stats.</p>
-                    <Link to="/showpage3"><button className="square-btn-big center x-large mt-3">
+                    <Link to="/showpage3"><button onClick={() => {resetScroll()}} className="square-btn-big center x-large mt-3">
                         <p className="m0 inline v-align mb-2 ml-3-4-respond">View</p>
                         <span className="material-symbols-outlined v-align ml-2 mb-2 white-text mt-h-respond">
                             arrow_forward
@@ -47,7 +47,7 @@ const MiniApps = (props) => {
                     </div>
                     <p className="m0 square-title large center-text my-1 dark-text"><strong>Geo-Weather App</strong></p>
                     <p className="m0 square-desc medium center-text black-text">Search any city to find out what the weather is like there right now.</p>
-                    <Link to="/showpage4"><button className="square-btn-big center x-large mt-3">
+                    <Link to="/showpage4"><button onClick={() => {resetScroll()}} className="square-btn-big center x-large mt-3">
                         <p className="m0 inline v-align mb-2 ml-3-4-respond">View</p>
                         <span className="material-symbols-outlined v-align ml-2 mb-2 white-text mt-h-respond">
                             arrow_forward
